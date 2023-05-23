c25b9ff77be71719c2961c07167e75805c174a12
maria-raquel <mariaraquelmartinez97@gmail.com>
2023-05-22 22:22:15 -0300

    modularizando o código

--- problema_4_cores.py
+++ problema_4_cores.py
@@ -1,25 +1,5 @@
 from copy import deepcopy
 
-Roraima = {
-    'Uiramuta': ['Pacaraima', 'Normandia'],
-    'Pacaraima': ['Uiramuta', 'Normandia', 'Boa Vista', 'Amajari'],
-    'Normandia': ['Uiramuta', 'Bonfim', 'Boa Vista', 'Pacaraima'],
-    'Boa Vista': ['Pacaraima', 'Normandia', 'Bonfim', 'Canta', 
-                  'Mucajai', 'Alto Alegre', 'Amajari'],
-    'Bonfim': ['Caracarai', 'Canta', 'Boa Vista', 'Normandia'],
-    'Canta': ['Bonfim', 'Caracarai', 'Iracema', 'Mucajai', 'Boa Vista'],
-    'Amajari': ['Pacaraima', 'Boa Vista', 'Alto Alegre'],
-    'Alto Alegre': ['Amajari', 'Boa Vista', 'Mucajai', 'Iracema'],
-    'Mucajai': ['Alto Alegre', 'Boa Vista', 'Canta', 'Iracema'],
-    'Iracema': ['Alto Alegre', 'Mucajai', 'Canta', 'Caracarai'],
-    'Caracarai': ['Iracema', 'Canta', 'Bonfim', 'Caroebe', 'Sao Joao da Baliza', 
-                  'Sao Luiz do Anaua', 'Rorainopolis'],
-    'Caroebe': ['Sao Joao da Baliza', 'Caracarai'],
-    'Sao Joao da Baliza': ['Rorainopolis', 'Sao Luiz do Anaua', 'Caracarai', 'Caroebe'],
-    'Rorainopolis': ['Caracarai', 'Sao Luiz do Anaua', 'Sao Joao da Baliza'],
-    'Sao Luiz do Anaua': ['Caracarai', 'Sao Joao da Baliza', 'Rorainopolis'],
-}
-
 class Problema:
     def __init__(self, bordas, cores):
         # bordas é um dicionário onde cada chave é uma região
@@ -33,6 +13,7 @@ class No:
     def __init__(self, mapa, regiao, cor):
         # mapa é um dicionario
         # cada chave é uma região, seu valor é sua cor atual
+        # ex: {'São Paulo': 'azul', 'Paraíba': 'vermelho'}
         # representa diferentes estados do sistema
         self.mapa = deepcopy(mapa)
 
@@ -41,6 +22,7 @@ class No:
         self.mapa[regiao] = cor
 
     def __str__(self):
+        # só imprime as regiões preenchidas, por legibilidade
         string = ''
         for regiao in self.mapa:
             if self.mapa[regiao] != '':
@@ -50,22 +32,63 @@ class No:
     def __repr__(self):
         return self.__str__()
     
-    def filhos(self, cores, proxima_regiao):
+    # Ramifica o nó, método a ser chamado na busca
+    # retorna uma lista com os filhos do nó
+    def filhos(self, problema, proxima_regiao):
         filhos = []
-        for cor in cores:
+        for cor in problema.cores:
             filho = No(self.mapa, proxima_regiao, cor)
             filhos.append(filho)
         return filhos
+    
+    # varre o mapa, verifica se há regiões fronteiriças com a mesma cor
+    # será usado para podar nós com estados inviáveis
+    def is_viavel(self, problema):
+        for regiao in self.mapa:
 
+            if self.mapa[regiao] == '':
+                return True
+            
+            for borda in problema.bordas[regiao]:
+                if self.mapa[regiao] == self.mapa[borda]:
+                    return False
+                
+        return True
+
+    # varre o mapa, verifica se todas as regiões estão preenchidas
+    def is_preenchido(self):
+        for regiao in self.mapa:
+
+            if self.mapa[regiao] == '':
+                return False
+        
+        return True
+    
+    '''
+    is_viavel verifica se o nó respeita a restrição do problema: 
+        não ter 4 cores iguais em regiões fronteiriças
+        mas não verifica se o mapa está completamente colorido
+
+    is_preenchido verifica se o mapa do nó está completamente colorido mas
+        não verifica se o nó é viável
+        
+    é importante usar os métodos juntos para determinar se o nó representa
+        o estado objetivo
+    '''
+
+COLORACAO_INICIANDO = 1
+COLORACAO_EM_ANDAMENTO = 2
+COLORACAO_SUCESSO = 3
+COLORACAO_SEM_SUCESSO = 0
+
+# classe para realizar a busca
 class Coloracao:
     def __init__(self, problema):
         self.problema = problema
-        self.fronteira = []
-        self.status = 'Coloração iniciando'
-        # status possíveis: 'Coloração iniciando', 'Coloração em andamento', 'Coloração finalizada'
+        self.fronteira = [] 
+        #  fronteira do problema de busca
 
-        self.indice = 0
-        # o indice guardará a posição da próxima região a ser colorida na busca
+        self.status = COLORACAO_INICIANDO
 
         # mapa é um dicionário com todas as regiões e suas cores
         # começa em branco
@@ -75,16 +98,62 @@ class Coloracao:
             self.mapa[regiao] = ''
 
     def passo(self):
-        if self.status == 'Coloração finalizada':
-            print('Coloração finalizada')
+        if self.status == COLORACAO_INICIANDO:
+
+            primeira_região = list(self.mapa.keys())[0]
+            # pegamos a primeira região do dicionário, que está todo em branco
+
+            # criaremos 4 nós, para as 4 possíveis cores para a primeira região
+            for cor in self.problema.cores:
+                novo_no = No(self.mapa, primeira_região, cor)
+                self.fronteira.append(novo_no)
+            
+            self.status = COLORACAO_EM_ANDAMENTO
+
             return
         
         try:
-            no = self.fronteira.pop(-1)
+            proximo_no = self.fronteira.pop(-1)
         except IndexError:
-            self.situacao = 'Coloração finalizada'
+            self.situacao = COLORACAO_SEM_SUCESSO
             return
 
-        for filho in no.filhos(self.problema.cores, self.problema.bordas[self.problema.mapa]):
-            self.fronteira.append(filho)
-            pass
+        proxima_regiao = next(regiao for regiao in proximo_no.mapa if proximo_no.mapa[regiao] == '')
+        # se der erro, é pq o mapa está cheio
+        # não dará erro, pois a cada passo é testado se o mapa dos nós a serem empilhados está cheio
+
+        for filho in proximo_no.filhos(self.problema, proxima_regiao):
+
+            # verificando se o filho é viavel antes de adicioná-lo na fronteira
+            if filho.is_viavel(self.problema):
+
+                # verificando se o filho é objetivo
+                if filho.is_preenchido():
+                    self.status = COLORACAO_SUCESSO
+                    self.mapa = filho.mapa
+                    return
+                
+                else: 
+                    self.fronteira.append(filho)
+        
+        return
+    
+    def executar(self):
+        while self.status == COLORACAO_EM_ANDAMENTO or self.status == COLORACAO_INICIANDO:
+            self.passo()
+        return 
+    
+    def show_status(self):
+        if self.status == COLORACAO_INICIANDO:
+            return 'Coloração iniciando'
+        elif self.status == COLORACAO_EM_ANDAMENTO:
+            return 'Coloração em andamento'
+        elif self.status == COLORACAO_SUCESSO:
+            return 'Coloração bem sucedida'
+        elif self.status == COLORACAO_SEM_SUCESSO:
+            return 'Coloração sem sucesso'
+        
+    def imprime_mapa_colorido(self):
+        for regiao in self.mapa:
+            print(f'{regiao}: {self.mapa[regiao]}')
+        return
