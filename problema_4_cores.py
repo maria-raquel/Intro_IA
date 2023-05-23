80d8e67916f8deca2246158a98bb8f70781ef670
maria-raquel <mariaraquelmartinez97@gmail.com>
2023-05-23 09:07:36 -0300

    adiciona a classe coloração em largura

--- problema_4_cores.py
+++ problema_4_cores.py
@@ -85,8 +85,110 @@ COLORACAO_EM_ANDAMENTO = 2
 COLORACAO_SUCESSO = 3
 COLORACAO_SEM_SUCESSO = 0
 
-# classe para realizar a busca
-class Coloracao:
+# classe para realizar a coloração, com busca em profundidade
+class Coloracao_Profundidade:
+    def __init__(self, problema):
+        self.problema = problema
+        self.fronteira = [] 
+        #  fronteira do problema de busca
+
+        self.status = COLORACAO_INICIANDO
+
+        # mapa é um dicionário com todas as regiões e suas cores
+        # começa em branco
+        # representa o estado inicial do sistema
+        self.mapa = {}
+        for regiao in self.problema.bordas:
+            self.mapa[regiao] = ''
+        
+        self.passos_dados = 0
+
+    def __str__(self):
+        if self.status == COLORACAO_INICIANDO:
+            return 'Coloração iniciando'
+        elif self.status == COLORACAO_EM_ANDAMENTO:
+            return 'Coloração em andamento'
+        elif self.status == COLORACAO_SUCESSO:
+            return 'Coloração bem sucedida'
+        elif self.status == COLORACAO_SEM_SUCESSO:
+            return 'Coloração sem sucesso'
+        
+    def __repr__(self):
+        return self.__str__()
+
+    def passo(self):
+        if self.status == COLORACAO_INICIANDO:
+
+            primeira_região = list(self.mapa.keys())[0]
+            # pegamos a primeira região do dicionário, que está todo em branco
+
+            # criaremos 4 nós, para as 4 possíveis cores para a primeira região
+            for cor in self.problema.cores:
+                novo_no = No(self.mapa, primeira_região, cor)
+                self.fronteira.append(novo_no)
+            
+            self.status = COLORACAO_EM_ANDAMENTO
+            self.passos_dados = 1
+
+            return
+        
+        try:
+            proximo_no = self.fronteira.pop(-1)
+        except IndexError:
+            self.situacao = COLORACAO_SEM_SUCESSO
+            return
+
+        proxima_regiao = next(regiao for regiao in proximo_no.mapa if proximo_no.mapa[regiao] == '')
+        # se der erro, é pq o mapa está cheio
+        # não dará erro, pois a cada passo é testado se o mapa dos nós a serem empilhados está cheio
+
+        for filho in proximo_no.filhos(self.problema, proxima_regiao):
+
+            # verificando se o filho é viavel antes de adicioná-lo na fronteira
+            if filho.is_viavel(self.problema):
+
+                # verificando se o filho é objetivo
+                if filho.is_preenchido():
+                    self.status = COLORACAO_SUCESSO
+                    self.mapa = filho.mapa
+                    return
+                
+                else: 
+                    self.fronteira.append(filho)
+        
+        self.passos_dados += 1
+        
+        return
+    
+    def executar(self):
+        while self.status == COLORACAO_EM_ANDAMENTO or self.status == COLORACAO_INICIANDO:
+            self.passo()
+        return 
+        
+    def imprime_mapa_colorido(self):
+        for regiao in self.mapa:
+            print(f'{regiao}: {self.mapa[regiao]}')
+        return
+    
+    def gera_mapa(self, geojson_path):
+        gdf = gpd.read_file(geojson_path)
+
+        # gera o mapa
+        fig, ax = plt.subplots(figsize=(10, 10))
+        gdf.plot(ax=ax, linewidth=0.8, edgecolor='white', legend=False)
+
+        # para cada região, adiciona um patch da cor correspondente
+        for regiao in self.mapa:
+            gdf[gdf.name == regiao].plot(edgecolor = 'white', color = self.mapa[regiao], ax=ax)
+
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+        plt.show()
+        return
+    
+# classe para realizar a coloração, com busca em largura
+class Coloracao_Largura:
     def __init__(self, problema):
         self.problema = problema
         self.fronteira = [] 
